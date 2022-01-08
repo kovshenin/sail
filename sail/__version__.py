e4d47dedcf74a16476c5416fd9b74ebf410d9234
Konstantin Kovshenin <kovshenin@gmail.com>
2022-01-08 15:12:12 +0300

    Bump version

--- sail/__version__.py
+++ sail/__version__.py
@@ -1,6 +1,6 @@
 __title__ = 'sail'
 __description__ = 'CLI tool to deploy and manage WordPress applications on DigitalOcean'
-__version__ = '0.10.3'
+__version__ = '0.10.4-beta1'
 __author__ = 'Konstantin Kovshenin'
 __author_email__ = 'kovshenin@gmail.com'
 __url__ = 'https://sailed.io'
